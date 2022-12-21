54f4feeb7e5d48192332403c36a29104cf027aee
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2022-12-21 10:35:20 -0800

    s

--- structs.h
+++ structs.h
@@ -1,37 +1,39 @@
 #include <vector>
 using namespace std;
-#include <string>
 #include <array>
-struct bird{
+#include <ctime>
+#include <string>
+struct bird {
 public:
-bird(){
- sethp();
-
-}
-void sethp(){
- for(int i=1;i<11;i++){
-health.push_back(i);
- }
-}
+  bird(char &playermodel) {
+    sethp();
+    this->playermodel = playermodel;
+    srand(time(NULL));
+  }
+  void sethp() {
+    for (int i = 1; i < 11; i++) {
+      health.push_back(i);
+    }
+  }
 
 private:
-vector <int> health;
-char playermodel;
-
-
-
-
+  vector<int> health;
+  char playermodel;
 };
 
-struct pipe{
- public:
-pipe(){
+struct pipe {
+public:
+  pipe(int rownum, int colheight, char model) {
+    // init default heights
+    // gives pipes their height
+    heightgen(colheight);
+    this->pipemodel = model;
+  }
 
+  void heightgen(int &colheight) { height = rand() % colheight; }
 
-}
- private:
-char pipemodel;
-// height should be pseudorandom within range of mod column height
-unsigned int height;
-
+private:
+  char pipemodel;
+  // height should be pseudorandom within range of mod column height
+  unsigned int height;
 };
