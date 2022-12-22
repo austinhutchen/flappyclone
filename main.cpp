d50527010b54b60f994a38d6b000fff04eac45d2
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2022-12-21 17:23:03 -0800

    s

--- main.cpp
+++ main.cpp
@@ -4,39 +4,40 @@
 int main() {
   cout << "=========FLAPPY BIRD V.1 ========" << endl;
   int level;
-  // this variable is re-used throughout program for inputs requiring integers
+  // the below variable is re-used throughout program for inputs requiring integers
   unsigned int choice;
-  char player;
-  string characters[4] = {"", "🦅", "🦇", "🦆"};
+  string characters[4] = {"", "🦅", "🦉", "🦆"};
   cout << "CHOOSE YOUR CHARACTER:" << endl;
   for (int i = 1; i < 4; i++) {
     cout << "("<<i << "). " << characters[i];
   }
   cout << endl;
   cin >> choice;
-
   switch (choice) {
   case 1: {
     bird b = bird(characters[1]);
     cout << b.getplayer() << " Chosen.." << endl;
     break;
   }
-
   case 2: {
     bird b = bird(characters[2]);
     cout << b.getplayer() << " Chosen.." << endl;
     break;
   }
-
   case 3: {
     bird b = bird(characters[3]);
     cout << b.getplayer() << " Chosen.." << endl;
     break;
   }
   default: {
-
     cout << "INVALID CHOICE. CHOOSE A CHARACTER from 1-3." << endl;
     return main();
   }
   }
+//player class initialized, now build map and send first output
+
+
+
+
+
 }
